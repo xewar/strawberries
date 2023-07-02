6c2e167a1a2fd78907b1d4e8a96c15e5c9ba9540
xewar <92114989+xewar@users.noreply.github.com>
2023-07-02 17:02:09 -0400

    initial setup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2baa762dda3458d6"; }
+/******/ 		__webpack_require__.h = function() { return "ae0bf2063717fe1c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
