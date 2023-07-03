505d7389d6ae8a5a6d2db61433dafdc56fbb1164
xewar <92114989+xewar@users.noreply.github.com>
2023-07-03 17:12:15 -0400

    created about + events pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import PlantsLayout from "./seeds";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
+      <div className="featuredPlant">
+        <Image
+          className="h-48 w-full object-cover md:h-full md:w-48"
+          src={`/../public/images/plants/new-england-aster`}
+          width={1024}
+          height={1024}
+        ></Image>
+      </div>
       <div className="text-3xl ">Hello world</div>
+      <PlantsLayout />
     </main>
   );
 }
