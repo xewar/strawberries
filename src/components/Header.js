bf44e0ab64c2515dda57432ad21b27084ef075fd
xewar <92114989+xewar@users.noreply.github.com>
2023-07-02 23:09:17 -0400

    creating responsive nav menu for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,46 @@
 import Link from "next/link";
 import { BsCart3 } from "react-icons/bs";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiMenu } from "react-icons/fi";
+import { LuFlower } from "react-icons/lu";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="header bg-sky-200 p-3 md:flex md:flex-row md:justify-between">
-      <div className="left text-center pb-6 md:text-left">
+    <div className="header bg-emerald-600 p-3 flex  justify-between md:flex-row md:justify-between">
+      <div className="left text-center md:text-left">
         <div className="title text-4xl ">wild strawberries</div>
         <div className="subtitle text-xl italic">a native plants store</div>
       </div>
       <div className="right flex flex-col  gap-2">
-        <div class="rightTop  flex flex-col items-end md:text-sm md:flex md:justify-between md:flex-row md:gap-4">
-          <div className="login">
-            <Link href="">Log in</Link> or{" "}
-            <Link href="">Create an account</Link>
-          </div>
-          <div className="cart">
-            <Link href="" className="flex gap-2 items-center">
-              <BsCart3 />
-              Cart
-            </Link>
+        <div class="loginMenu flex justify-end">
+          <LuFlower className="text-3xl md:hidden" />
+          <div class="rightTop hidden md:flex flex-col items-end md:text-sm md:flex md:justify-between md:flex-row md:gap-4">
+            <div className="login">
+              <Link href="">Log in</Link> or{" "}
+              <Link href="">Create an account</Link>
+            </div>
+            <div className="cart">
+              <Link href="" className="flex gap-2 items-center">
+                <BsCart3 />
+                Cart
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="menu flex flex-col uppercase text-right gap-1 text-lg">
-          <Link href="">Seeds</Link>
-          <Link href="">Events</Link>
-          <Link href="">Publications</Link>
+
+        <div class="navMenu flex justify-end">
+          <FiMenu className="text-3xl md:hidden" />
+          <div className=" hidden menu md:flex flex-col uppercase text-right gap-1 text-lg">
+            <Link href="">Seeds</Link>
+            <Link href="">Events</Link>
+            <Link href="">Publications</Link>
+          </div>
         </div>
         <div className="search flex justify-end gap-2">
           <input
-            className="focus:outline-none p-1 rounded-md"
+            className=" hidden focus:outline-none p-1 rounded-md"
             type="search"
             name="search"
           />
