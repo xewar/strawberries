c4105b06187ce98ade095c521e936cea4815cac5
xewar <92114989+xewar@users.noreply.github.com>
2023-07-04 16:40:49 -0400

    filled in about page with information about the project

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,88 @@
+import Image from "next/image";
+import Link from "next/link";
+// import { GiDandelionFlower } from "react-icons/gi";
+// import { PiPlantDuotone } from "react-icons/pi";
+import { GoNorthStar } from "react-icons/go";
+
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div className="md:flex md:gap-4 md:pr-12">
+      <div className="left md:max-w-2xl  pb-4 ">
+        <Image
+          className="image object-cover md:h-2/3"
+          alt="butterfly milkweed flowers"
+          src={`/../public/images/plants/butterfly-milkweed.jpg`}
+          width={1024}
+          height={1024}
+        ></Image>
+        <div className="p-2 imageCaption text-sm">
+          <div className="plantTitle italic">butterfly milkweed</div>
+          <div className="plantInfo">
+            A summer-blooming wildflower in the milkweed family that is an
+            important host to the monarch butterfly.
+          </div>
+        </div>
+      </div>
+      <div className="right flex flex-1 flex-col p-2 gap-4">
+        <div className="aboutTitle italic text-emerald-600 text-lg">
+          wild strawberries - a native seed store
+        </div>
+        <div>
+          This is an example website inspired by the{" "}
+          <Link
+            className="text-emerald-600"
+            href="https://shop.wildseedproject.net/"
+          >
+            Wild Seed Project
+          </Link>
+          , a Maine-based nonprofit focused on restoring native plants to the
+          Maine landscape. All the photos and descriptions of plants are
+          courtesy of them.
+        </div>
+        <div>
+          You can purchase any of the seeds featured on this site find out more
+          about their work on their{" "}
+          <Link
+            className="text-emerald-600"
+            href="https://shop.wildseedproject.net/"
+          >
+            website
+          </Link>
+          .
+        </div>
+        <div>
+          My learning goals for this project were to:
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" />
+            Deepen my fluency with React
+          </div>
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" /> Familiarize myself with Next.js
+            + Tailwind CSS
+          </div>
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" /> Implement user authentication
+            with Passport.js
+          </div>
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" /> Learn to use Postgres + Prisma
+          </div>
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" /> Design a visually polished
+            fullstack app
+          </div>
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" /> Build user-friendly custom
+            search support
+          </div>
+          <div className="flex gap-4 items-center">
+            <GoNorthStar className="text-xs" /> Continue to work on good UI
+            practices, accessibility, and responsive web design.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
