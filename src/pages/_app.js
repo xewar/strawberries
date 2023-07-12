914c26c6210aba706def82ec7de1807531641afe
xewar <92114989+xewar@users.noreply.github.com>
2023-07-12 18:37:40 -0400

    wrapped app in LanguageContextProvider for english-lenape state management

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import Header from "@/components/Header";
+import { LanguageContextProvider } from "../contexts/LanguageContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
-    </div>
+    <LanguageContextProvider>
+      <div className="text-yellow-800 font-neue-haas tracking-wider">
+        <Header />
+        <Component {...pageProps} />
+        <div className="noise fixed bg-opacity-30 top-0 h-full w-full pointer-events-none"></div>
+      </div>
+    </LanguageContextProvider>
   );
 }
