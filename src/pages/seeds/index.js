3fb5ef1e63ca598312ce7b958e5213232721c25b
xewar <92114989+xewar@users.noreply.github.com>
2023-07-12 18:39:16 -0400

    Added Toggle to PlantsLayout on the homepage

--- src/pages/seeds/index.js
+++ src/pages/seeds/index.js
@@ -1,12 +1,17 @@
 "use client";
 import PlantsList from "@/pages/seeds/PlantsList";
 import plantData from "@/pages/seeds/plantData";
+import Toggle from "@/components/Toggle";
+import React, { useState } from "react";
 
 function PlantsLayout({ children }) {
   const plants = plantData.wildflowers;
 
   return (
-    <div>
+    <div className="flex flex-col">
+      <div className="flex  justify-end p-4 items-center">
+        <Toggle />
+      </div>
       <PlantsList plants={plants} />
       {children}
     </div>
