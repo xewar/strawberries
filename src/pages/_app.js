d6360246ad103844fd9b01459df5f7182fe8e08e
xewar <92114989+xewar@users.noreply.github.com>
2023-07-16 17:06:17 -0400

    debugging noise filter issue in mobile deployment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,8 +8,8 @@ export default function App({ Component, pageProps }) {
       <div className="text-yellow-800 font-neue-haas tracking-wider">
         <Header />
         <Component {...pageProps} />
-        <div className="z-50 noise fixed bg-opacity-30 top-0 h-full w-full pointer-events-none"></div>
       </div>
+      <div className="z-50 noise fixed  top-0 h-full w-full pointer-events-none"></div>
     </LanguageContextProvider>
   );
 }
