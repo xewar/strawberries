b93c339420b2320d6a071ec3d4aa1e5951b61942
xewar <92114989+xewar@users.noreply.github.com>
2023-07-16 16:12:07 -0400

    display image on individual plant page

--- src/pages/seeds/[fileExt].js
+++ src/pages/seeds/[fileExt].js
@@ -2,37 +2,50 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import plantData from "@/pages/seeds/plantData";
+import PlantData from "@/lib/PlantData";
+import styles from "@/styles/toggle.module.css";
 
 function IndividualPlantInfo() {
   //temp solution before I move plant data to database
   const { query } = useRouter();
   let fileExt = query.fileExt;
   const findFlowerByFileExt = (fileExt) => {
-    const flower = plantData.wildflowers.find(
+    const flower = PlantData.wildflowers.find(
       (flower) => flower.fileExt === fileExt
     );
     return flower;
   };
   const plant = findFlowerByFileExt(fileExt);
+
   return (
-    <div>
-      <div className="left">
-        <Link href="/seeds" className="top">
-          Back to all plants page
+    <div className="pageContent flex bg-[#ff0] ">
+      <div className="left w-1/2 ">
+        <Link
+          href="/seeds"
+          className={`backButton top flex flex-col w-[140px] mx-11 -my-12 text-center ${styles.button}`}
+        >
+          <div>Back to</div> <div>all plants</div>
         </Link>
-        <div className="plantPhoto md:w-2/3">
-          <Image
-            src={`/../public/images/plants/${plant?.fileExt}.jpg`}
-            width={1024}
-            height={1024}
-            sizes=""
-            alt={`Picture of ${plant?.englishName}`}
-            href=""
-          ></Image>
+        <div className="image-container w-3/4 ml-24 mt-4 ">
+          <svg viewBox="0 0 381.5 375.9" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <clipPath id="splotch3">
+                <path
+                  d="M376.7,166.8C384,277.9,315,375.9,183.4,356.7C39,351.9-89,167.9,103.2,41.9C183-26,370.9,78.5,376.7,166.8z"
+                  fill=""
+                />
+              </clipPath>
+            </defs>
+            <g clipPath="url(#splotch3)">
+              <image
+                className={`w-full bg-white bg-opacity-100 hover:bg-rose-400 hover:scale-105 `}
+                xlinkHref={`/images/plants/${plant?.fileExt}.jpg`}
+                style={{ objectPosition: "top right" }}
+              />
+            </g>
+          </svg>
         </div>
       </div>
-
       <div className="right">
         <div>{plant?.englishName}</div>
       </div>
