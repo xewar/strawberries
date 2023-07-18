bdc56bb8f3e56938bf7b87f17485a8811b6caa5e
xewar <92114989+xewar@users.noreply.github.com>
2023-07-17 22:48:46 -0400

    updated viewport settings

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
 import { LanguageContextProvider } from "../contexts/LanguageContext";
@@ -5,6 +6,9 @@ import { LanguageContextProvider } from "../contexts/LanguageContext";
 export default function App({ Component, pageProps }) {
   return (
     <LanguageContextProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <div className="text-yellow-800 font-neue-haas tracking-wider">
         <Header />
         <Component {...pageProps} />
