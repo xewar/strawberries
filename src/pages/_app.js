fd0d9af1f63fbaf1625a20a09efb43763f95a6bd
xewar <92114989+xewar@users.noreply.github.com>
2023-08-03 19:41:48 -0400

    added Redux to manage shopping cart

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,26 @@
 import Head from "next/head";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
 import { LanguageContextProvider } from "../contexts/LanguageContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <LanguageContextProvider>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <div className="text-yellow-800 font-neue-haas tracking-wider">
-        <Header />
-        <Component {...pageProps} />
-      </div>
-      <div className="z-50 noise fixed  top-0 h-full w-full pointer-events-none"></div>
-    </LanguageContextProvider>
+    <Provider store={store}>
+      <LanguageContextProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <div className="text-yellow-800 font-neue-haas tracking-wider">
+          <Header />
+          <Component {...pageProps} />
+        </div>
+        <div className="z-50 noise fixed  top-0 h-full w-full pointer-events-none"></div>
+      </LanguageContextProvider>
+    </Provider>
   );
 }
